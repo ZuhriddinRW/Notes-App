5da6f4f9c9b04d72f30df54b2d7d157ef7afc7ba
Abdullac <abdullacoder17@gmail.com>
2026-03-07 20:32:12 +0500

    Added TailwindCSS

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
