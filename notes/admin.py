d9f14def393a44a88a6e03681c4f303f810a3357
ZuhriddinRW <shodmonovzuhriddin2009@gmail.com>
2026-03-14 11:38:47 +0500

    changed name of users into accounts

--- notes/admin.py
+++ notes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import Group
-from users.models import User
+from accounts.models import User
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from unfold.admin import ModelAdmin
 from .models import Category, Note
